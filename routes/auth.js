fbd98327aed23964bb5499858c6df95c031068fe
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-07 19:26:02 +0400

    import style refactoring

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { omit } = require('ramda');
-const { Auth } = require('../services/index');
+const { Auth } = require('../services');
 
 const router = express.Router();
 
