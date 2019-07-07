eb76dd85ff059ff9274b1683eecd4e5623e325ab
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-07 16:20:25 +0400

    implement auth logic in router

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { omit } = require('ramda');
 const { Auth } = require('../services/index');
 
 const router = express.Router();
 
 
-router.get('register', onRegisterRequest);
+router.post('register', onRegisterRequest);
+router.post('auth', onAuthRequest);
 
 async function onRegisterRequest(req, res, next) {
     const { username, password } = req.body;
@@ -14,6 +16,19 @@ async function onRegisterRequest(req, res, next) {
         return res.json(result);
     } catch (err) {
         console.error(err);
+        return next(err);
+    }
+}
+
+async function onAuthRequest(req, res, next) {
+    const { username, password } = req.body;
+    try {
+        const user = await Auth.auth({ username, password });
+        const token = Auth.getAuthToken(omit(['password'], user), {
+            ipAddress: req.connection.remoteAddress,
+        });
+        return res.json({ token });
+    } catch (err) {
         return next(err);
     }
 }
