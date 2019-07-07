c075f61c6d85244f4394f9f8177c61f494fc53aa
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-07 17:00:34 +0400

    setup first routes and test

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const models = require('./models/sequelize');
+const routes = require('./routes/index');
 const config = require('./config');
 
 
 models.init();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('env', config.server.env);
 if (config.server.enableCors) {
@@ -17,6 +21,9 @@ if (config.server.enableCors) {
 app.get('', (req, res) => {
     res.send('test, test');
 });
+
+// setup routes
+app.use('/api/auth', routes.Auth);
 
 app.listen('3000', () => {
     console.log('server is running at port 3000');
