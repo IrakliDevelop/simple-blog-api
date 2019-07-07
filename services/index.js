a92b617fc7285593ca14343a1059d307db9822b4
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-07 17:00:26 +0400

    remove underscore from services object name

--- services/index.js
+++ services/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const { capitalize } = require('../utils');
 
-const _services = {
+const services = {
 
 };
 
@@ -11,8 +11,8 @@ const _services = {
 fs.readdirSync(__dirname).forEach(fname => {
     if (fname.match(/\.js$/) !== null && fname !== 'index.js') {
         const sname = capitalize(fname.replace('.js', ''));
-        _services[sname] = require(path.join(__dirname, fname));
+        services[sname] = require(path.join(__dirname, fname));
     }
 });
 
-module.exports = _services;
+module.exports = services;
