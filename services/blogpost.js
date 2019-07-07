ff378b4b7e87dda67d4739c781a702759812e21a
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-07 19:36:59 +0400

    add blog update functionality

--- services/blogpost.js
+++ services/blogpost.js
@@ -21,12 +21,38 @@ module.exports.createBlogPost = async function (blogPost) {
     if (!blogPost.content) {
         throw new CustomError({
             message: 'BlogPost content mustn\'t be empty',
-            status: 406,
-            code: 'ERROR_BLOGPOST_EMPTY_CONTENT',
+            status: 'ERROR_BLOGPOST_EMPTY_CONTENT',
+            code: 406,
         });
     }
     try {
         await blogPostModel.createBlogPost(blogPost);
+        return {
+            status: 'OK',
+            code: 200,
+        };
+    } catch (err) {
+        console.error(err);
+        return {
+            code: 503,
+            error: 'Internal server error',
+        };
+    }
+};
+
+module.exports.updateBlogPost = async function (blogPost) {
+    const blogPostModel = models.getModel('Blogpost');
+
+    if (!blogPost.content) {
+        throw new CustomError({
+            message: 'BlogPost content mustn\'t be empty',
+            status: 'ERROR_BLOGPOST_EMPTY_CONTENT',
+            code: 406,
+        });
+    }
+    try {
+        const foundBlogPost = blogPostModel.findById(blogPost.id);
+        await foundBlogPost.updateBlogPost(blogPost);
         return {
             status: 'OK',
             code: 200,
