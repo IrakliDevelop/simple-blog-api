6a130831ba5a800af6cb13f540b43b323fc0e0f3
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-07 13:09:38 +0400

    add requireAuth middleware

---
+++ middlewares/requireAuth.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const getToken = authorization => {
+    if (!authorization) return null;
+    if ((authorization || '').startsWith('Bearer')) {
+        return authorization.split(' ')[1];
+    }
+    return authorization;
+};
+
+const verifYToken = token => {
+    return new Promise((resolve, reject) => {
+        if (!token) return reject();
+
+        return jwt.verify(token, config.jwt.secret, (err, decoded) => {
+            if (err) return reject(err);
+            return resolve(decoded);
+        });
+    });
+};
+
+const onUnauthorized = res => {
+    res.status(401).end('Unauthorized');
+};
+
+module.exports = (req, res, next) => {
+    const token = getToken(req.headers.authorization);
+    if (!token) return onUnauthorized(res);
+
+    return verifYToken(token).then(decoded => {
+        req.user = decoded;
+        return next();
+    }).catch(() => {
+        return onUnauthorized(res);
+    });
+};
