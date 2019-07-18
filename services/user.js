f71ecbc8e590291af4896603d4fd8c4a979f8d6c
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-18 23:23:00 +0400

    create user service

---
+++ services/user.js
@@ -0,0 +1,18 @@
+const { CustomError } = require('../utils');
+const models = require('../models/sequelize');
+
+/**
+ * returns user info based on provided username
+ * @param {string} username
+ * @returns {Promise<*>}
+ */
+module.exports.getUserInfo = async function (username) {
+    const userModel = models.getModel('User');
+
+    try {
+        return await userModel.findByName(username);
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
